ceb23e3e5e6f5513655fbf724e2699ec690ea965
Jorge Aldana <jorge.aldana@ultertechnologies.com>
2020-05-20 17:46:16 -0500

    Agregando router
    Agregando componentes agradecimientos

---
+++ src/views/Agradecimientos.vue
@@ -0,0 +1,20 @@
+<template>
+    <v-app>
+        <h1>Muchas gracias por comprar con nosotros!</h1>
+        <router-link to="/">Regresar al formulario.</router-link>
+    </v-app>
+
+</template>
+
+<script>
+    export default {
+        name: "Agradecimientos",
+        data() {
+            return {}
+        }
+    }
+</script>
+
+<style scoped>
+    @import "/src/assets/estilos.css";
+</style>
