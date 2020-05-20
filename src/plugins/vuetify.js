da83dea6373bed571ab8277ec730435f9b5d07dc
Jorge Aldana <jorge.aldana@ultertechnologies.com>
2020-05-20 16:15:23 -0500

    Ajustando fondo, cambiando Icono
    Probando validaciones
    Ajustando colores de fondo, alerts y demás componentes

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -11,8 +11,8 @@ export default new Vuetify({
       },
     themes: {
       light: {
-        primary: '#ee44aa',
-        secondary: '#424242',
+        primary: '#86209f',
+        secondary: '#EEE',
         accent: '#82B1FF',
         error: '#FF5252',
         info: '#2196F3',
