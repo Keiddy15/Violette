43edf2c4a61280202897dd7c09d0d232982d2d9a
Jorge Aldana <jorge.aldana@ultertechnologies.com>
2020-05-20 23:43:13 -0500

    Cambiando Estilos, manejo de router

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,29 @@
 import Vue from 'vue'
+import Formulario from '../views/App'
+import Agradecimientos from '../views/Agradecimientos'
+import Bienvenida from "../views/Bienvenida";
 import VueRouter from 'vue-router'
 
 
 Vue.use(VueRouter)
 
-  const routes = [
-  /*{
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import('../views/About.vue')
-  }*/
-]
+const routes = [
+    {
+      path: '/',
+      name: 'Bienvenida',
+      components: {main: Bienvenida}
+    },
+    {
+      path: '/formulario',
+      name: 'app',
+      components: {main: Formulario}
+    },
+    {
+        path: '/thank-you',
+        name: 'Agradecimientos',
+        components: {main: Agradecimientos}
+    }
+];
 
 const router = new VueRouter({
   mode: 'history',
---
+++ src/views/Agradecimientos.vue
@@ -0,0 +1,46 @@
+<template>
+    <v-app class="principal">
+        <v-card class="cardForm2" shaped elevation="20">
+            <v-list-item three-line>
+                <v-list-item-content>
+                    <v-img
+                            src="https://firebasestorage.googleapis.com/v0/b/violette-8b112.appspot.com/o/LogoAgradecimientos.png?alt=media&token=49b8a018-fd72-4a8e-88d4-9d17e6f9b73b"
+                            class="logo2"
+                            contain
+                    >
+                    </v-img>
+                    <v-card-text>
+                        <div class="text-center"><span class="headline font-weight-bold">¡Pedido terminado!</span></div>
+                        <div class="text-center">En las próximas 24 horas estarás recibiendo tu numero de guía.</div>
+                        <div class="text-center">ESPERAMOS NOS COMPARTAS UNA FOTO PARA QUE HAGAS PARTE DE NUESTRO <span class="font-weight-bold">#TEAMVIOLETTE.</span></div>
+                    </v-card-text>
+                    <v-card-actions class="justify-center">
+                        <v-btn color="purple white--text" disabled block large @click="close">
+                            ¡Ya puedes cerrar esta pestaña!
+                        </v-btn>
+                        <router-link to="/">
+                        </router-link>
+                    </v-card-actions>
+                </v-list-item-content>
+            </v-list-item>
+        </v-card>
+    </v-app>
+</template>
+
+<script>
+    export default {
+        name: "Agradecimientos",
+        data() {
+            return {}
+        },
+        methods: {
+            close: function () {
+                window.close();
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    @import "/src/assets/estilos.css";
+</style>
