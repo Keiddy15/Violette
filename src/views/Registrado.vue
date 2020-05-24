4c2e1641183ca792b2b23b561587ee6d5cbee86a
Jorge Aldana <jorge.aldana@ultertechnologies.com>
2020-05-24 00:46:39 -0500

    Obteniendo info de Auth

--- src/views/Registrado.vue
+++ src/views/Registrado.vue
@@ -3,19 +3,16 @@
         <Toolbar/>
         <v-card class="cardForm2" elevation="10" shaped>
             <v-card-text>
-                <h1 class="text-center black--text">USTED HA SIDO REGISTRADO</h1>
-            </v-card-text>
-            <v-card-text>
-                <h2 class="text-center black--text">Haga click en ingresar</h2>
+                <p class="black--text" style="font-size: 22px; font-weight: bold;">Usted se ha registrado satisfactoriamente.</p>
+                <span class="text-center black--text">Haga click en ingresar, ¡Para acceder a su cuenta y realizar pedidos!</span>
             </v-card-text>
             <v-card-actions class="justify-center">
-                <v-btn text color="purple" @click="ingresar">
+                <v-btn block color="purple" class="white--text" @click="ingresar">
                     Ingresar
                 </v-btn>
             </v-card-actions>
         </v-card>
     </v-app>
-
 </template>
 
 <script>
